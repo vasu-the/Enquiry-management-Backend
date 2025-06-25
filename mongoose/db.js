01fb8392c269ce796a0d50133437395a636a847d
vasu-the <vasuthraganesh@gmail.com>
2025-06-25 14:45:06 +0530

    first commit

--- mongoose/db.js
+++ mongoose/db.js
@@ -6,8 +6,6 @@ mongoose.connect(process.env.MONGODBURL,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-     serverSelectionTimeoutMS: 10000, // Timeout after 10 seconds if no DB server found
-  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
   }).then(() => {
     console.log("Connected to MongoDB")
   })
