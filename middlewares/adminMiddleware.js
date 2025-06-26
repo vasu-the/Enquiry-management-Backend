989026d40b302904524029d2572e6d03414a6944
vasu-the <vasuthraganesh@gmail.com>
2025-06-26 08:26:25 +0530

    first commit

--- middlewares/adminMiddleware.js
+++ middlewares/adminMiddleware.js
@@ -9,13 +9,11 @@ module.exports = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     if (decoded.role !== "admin") {
-      return res.status(403).json({ message: "Access denied: not an admin" });
+      return res.status(403).json({ message: "Access denied: not admin" });
     }
-
     req.admin = true;
     next();
-  } catch (err) {
-    console.error("Token verification error:", err);
+  } catch {
     return res.status(401).json({ message: "Invalid or expired token" });
   }
 };
